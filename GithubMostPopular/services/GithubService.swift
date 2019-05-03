102d0889867e020b469d03c11434852ba2c60d98
Bruno Garcia <bcsgarcia@outlook.com>
2019-05-03 10:40:09 -0300

    Criado classe Service que consome a API do Github usando o Alamofire

---
+++ GithubMostPopular/services/GithubService.swift
@@ -0,0 +1,40 @@
+//
+//  GithubService.swift
+//  GithubMostPopular
+//
+//  Created by Garcia, Bruno (B.C.) on 02/05/19.
+//  Copyright © 2019 Garcia, Bruno (B.C.). All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+protocol GithubServiceProtocol {
+    func fetchStarredRepo(page: Int, completion: @escaping (GitResponse?, RequestError?) -> ())
+}
+
+class GithubService : GithubServiceProtocol {
+   
+    func fetchStarredRepo(page: Int, completion: @escaping (GitResponse?, RequestError?) -> ()) {
+        Alamofire.request(UrlRouter.getStarred(page))
+            .responseJSON { (response) in
+                if response.result.value == nil {
+                    completion(nil, .noResponse)
+                    return
+                }
+                guard let data = response.data else {
+                    completion(nil, .noData)
+                    return
+                }
+                
+                do {
+                    let mostPopular = try JSONDecoder().decode(GitResponse.self, from: data)
+                    DispatchQueue.main.async {
+                        completion(mostPopular, nil)
+                    }
+                } catch let jsonErr {
+                    print("Failed to decode:", jsonErr)
+                }
+        }
+    }
+}
