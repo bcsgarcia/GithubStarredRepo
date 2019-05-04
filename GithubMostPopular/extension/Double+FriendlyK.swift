f9c1b1885c7a9e8f33ac41d5b655eac3da6cde4d
Bruno Garcia <bcsgarcia@outlook.com>
2019-05-03 22:34:00 -0300

    Extension responsável por tratar o Star e Fork count para mostrar o numero de uma forma amigável. Ex: 29,5k

---
+++ GithubMostPopular/extension/Double+FriendlyK.swift
@@ -0,0 +1,47 @@
+//
+//  Double+FriendlyK.swift
+//  GithubMostPopular
+//
+//  Created by Garcia, Bruno (B.C.) on 03/05/19.
+//  Copyright © 2019 Garcia, Bruno (B.C.). All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func truncate(places: Int) -> Double {
+        return Double(floor(pow(10.0, Double(places)) * self)/pow(10.0, Double(places)))
+    }
+}
+
+func formatNumber(_ n: Int) -> String {
+    
+    let num = abs(Double(n))
+    let sign = (n < 0) ? "-" : ""
+    
+    switch num {
+        
+    case 1_000_000_000...:
+        var formatted = num / 1_000_000_000
+        formatted = formatted.truncate(places: 1)
+        return "\(sign)\(formatted)B"
+        
+    case 1_000_000...:
+        var formatted = num / 1_000_000
+        formatted = formatted.truncate(places: 1)
+        return "\(sign)\(formatted)M"
+        
+    case 1_000...:
+        var formatted = num / 1_000
+        formatted = formatted.truncate(places: 1)
+        return "\(sign)\(formatted)K"
+        
+    case 0...:
+        return "\(n)"
+        
+    default:
+        return "\(sign)\(n)"
+        
+    }
+    
+}
