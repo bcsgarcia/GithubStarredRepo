e4c4416bbdb1de16d85500096833846e1077738a
Bruno Garcia <bcsgarcia@outlook.com>
2019-05-03 22:32:52 -0300

    Adicionado propriedades fullName, forkCount, description e url do repositório.

--- GithubMostPopular/model/Repository.swift
+++ GithubMostPopular/model/Repository.swift
@@ -10,12 +10,21 @@ import Foundation
 
 class Repository: Codable {
     var name: String?
+    var fullName: String?
     var owner: User?
     var starCount: Int?
+    var forkCount: Int?
+    var url : String?
+    var description : String?
+    
     
     enum CodingKeys:String,CodingKey {
         case name = "name"
+        case fullName = "full_name"
         case owner = "owner"
         case starCount = "stargazers_count"
+        case forkCount = "forks_count"
+        case url = "html_url"
+        case description = "description"
     }
 }
